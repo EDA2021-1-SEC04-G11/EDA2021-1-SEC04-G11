9aebcf4a3d7a36132ee49b0c93f1a5be9afc2d7c
Lina-go <l.gomez1@uniandes.edu.co>
2021-02-07 18:51:28 -0500

    cambios del estudiante 2

--- App/view.py
+++ App/view.py
@@ -59,6 +59,7 @@ def loadTags():
     """
     return controller.loadTags('GoodReads/tags.csv')
 
+
 def loadBookTags():
     """
     Cargar los Tags de libros
@@ -79,8 +80,8 @@ while True:
 
         # TO-DO: Modificación de Est-1 en el Lab 2
 
-
         # TO-DO: Modificación de Est-2 en el Lab 2
+        print("último libro cargado:" + str(lt.lastElement(books)))
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
@@ -88,6 +89,10 @@ while True:
         print('Total de tags cargados: ' + str(lt.size(tags)))
     elif True:
         # TO-DO: Modificación de Est-2 en el Lab 2
+    elif int(inputs[0]) == 3:
+        print("Cargando información Book-Tags...")
+        booktags = loadBookTags()
+        print("Total de Book-tags cargados:" + str(lt.size(booktags)))
         pass
 
     else:
